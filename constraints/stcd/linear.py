dac7c765ebe902bac6553c4816ddc40753adc71d
Aries99C <aries99c@gmail.com>
2023-11-15 23:49:26 +0800

    完成时窗线性约束的挖掘

--- constraints/stcd/linear.py
+++ constraints/stcd/linear.py
@@ -1,3 +1,4 @@
+import math
 import random
 import numpy as np
 
@@ -39,15 +40,15 @@ class Linear:
             for i, col in enumerate(self.mts.cols):
                 self.variables.append((t, col, i))   # 每个变量的形式为(时间戳，列名称，列索引)
 
-    def mini_mine_sh(self, x, y, x_vars, y_var, verbose=0):
+    def mini_mine_sh(self, x, y, x_vars, y_var, max_x=5, verbose=0):
         # 生成随机掩码集合来学习规则
-        x_possible = set()      # 可能存在关联的变量X集合
-        while len(x_possible) < 4096:
-            mask = self.random_mask(len(x_vars))    # 获得随机掩码
-            if 1 not in mask:   # 随机选择没有选到任何变量X
+        x_possible = set()                      # 可能存在关联的变量X集合
+        while len(x_possible) < 1024:
+            mask = random_mask(len(x_vars), max_x=max_x)    # 获得随机掩码
+            if '1' not in mask:                 # 随机选择没有选到任何变量X
                 continue
             else:
-                x_possible.add(mask.__str__())
+                x_possible.add(mask)
         # 使用sklearn学习线性模型
         while len(x_possible) > self.n_components:  # 一直筛选直到得到参数限定的掩码个数
             n_possible = len(x_possible)
@@ -55,8 +56,10 @@ class Linear:
             budget = int(x.shape[0] / (n_possible * np.log2(n_possible)) + 10)
             x_train = x[:budget, :]         # 训练数据x
             y_train = y[:budget]            # 训练数据y
-            for mask_str in x_possible:     # 对每个掩码都学习一次线性函数
-                mask = np.array(mask_str)   # 将掩码字符串转化为ndarray
+            for i, mask_str in enumerate(x_possible):     # 对每个掩码都学习一次线性函数
+                mask = str2mask(mask_str)   # 将字符串转换为list
+                if mask is None:            # 有bug莫名其妙多一个解析不了的字符串
+                    continue
                 selected_x_train = None
                 # 由掩码生成训练数据
                 for idx in range(len(mask)):
@@ -65,23 +68,59 @@ class Linear:
                             selected_x_train = x_train[:, idx]
                         else:                           # 合并后加入的训练数据
                             selected_x_train = np.c_[selected_x_train, x_train[:, idx]]
-                if len(selected_x_train) == 1:          # 如果只选中一列X，需要reshape成2d数据
+                if len(selected_x_train.shape) == 1:    # 如果只选中一列X，需要reshape成2d数据
                     selected_x_train = selected_x_train.reshape(-1, 1)
                 # 训练线性模型
                 model = linear_model.Ridge(self.pc)
                 model.fit(selected_x_train, y_train)
                 # 记录线性模型的拟合误差
                 loss = np.sum((y_train - model.predict(selected_x_train)) ** 2)
-                mask_loss[mask.__str__()] = loss
+                mask_loss[mask_str] = loss
             # 根据误差重新筛选随机掩码
-            sorted_mask_str = sorted(mask_loss.items(), key=lambda x: x[1], reverse=False)  # 根据误差升序排列掩码
+            sorted_mask_str = sorted(mask_loss.items(), key=lambda kv: kv[1], reverse=False)  # 根据误差升序排列掩码
             x_possible = set()
             while len(x_possible) < n_possible / 2:     # 删除一半的掩码
-                x_possible.add(sorted_mask_str[len(x_possible)])
+                x_possible.add(sorted_mask_str[len(x_possible)][0])
+        if verbose > 0:     # 日志显示
+            idx = 1
+            for mask_str in x_possible:
+                print('变量X集合掩码{}: {}'.format(idx, np.array(mask_str)))
+                idx = idx + 1
         # 使用筛选后的掩码生成规则
-        # TODO
+        for mask_str in x_possible:
+            x_idx = []
+            x_names = []
+            mask = str2mask(mask_str)       # 将字符串转换为list
+            for idx in range(len(mask)):    # 得到掩码对应的变量X集合
+                if mask[idx] == 1:
+                    x_idx.append(idx)
+                    x_names.append(x_vars[idx])
+            # 由掩码和模型参数计算生成规则所需的数据
+            m, n = x.shape
+            x_mine = x[:int(m * self.sample_train), x_idx]
+            y_mine = y[:int(m * self.sample_train)]
+            # 挖掘规则
+            model = linear_model.Ridge(self.pc)
+            model.fit(x_mine, y_mine)
+            # 将规则转换成数据结构
+            func = {    # 记录挖掘的函数信息
+                'type': 'linear',                                       # 记录挖掘的函数类型
+                'coef': [model_coef for model_coef in model.coef_],     # 记录变量X的系数
+                'intercept': model.intercept_,                          # 函数的截距
+            }
+            # 根据置信度计算函数上下界
+            losses = abs(model.predict(x_mine) - y_mine)    # 模型误差
+            mean_loss = np.mean(losses)                     # 平均误差
+            b = max(losses)                                 # 最大误差
+            gamma = mean_loss
+            +b * math.sqrt(math.log(1/(1-self.confidence))/(2*m))
+            +b * math.sqrt(2*(len(x_vars)+1)*((math.log((math.e*m)/(2*(len(x_vars)+1))))/m))
+            r_mine = Rule(x_names, y_var, func, -gamma, gamma, model)   # 解析规则
+            if verbose > 0:     # 展示规则
+                print(r_mine)
+            self.rules.append(r_mine)                                   # 添加规则
 
-    def mine(self, verbose=0):
+    def mine(self, max_x=5, verbose=0):
         if verbose > 0:         # 日志显示
             print('{:=^80}'.format(' 在数据集{}上挖掘线性时窗约束 '.format(self.mts.dataset.upper())))
         d = array2window(self.mts.clean2array(), self.win_size)     # 对多元时序数据切片
@@ -96,25 +135,7 @@ class Linear:
             y = d[:, i]     # 变量Y的数据
             x = np.c_[d[:, 0: i], d[:, i+1:]] if i > 0 else d[:, i+1:]  # 变量X集合的数据
             # 挖掘时窗函数规则
-            self.mini_mine_sh(x, y, x_vars, y_var, verbose=verbose)
-
-    def random_mask(self, vec_len):
-        """
-        随机生成长度为vec_len的01掩码串
-        :param vec_len: 生成的掩码长度
-        :return: 长度为vec_len的随机01掩码串
-        """
-        mask = np.zeros(vec_len, dtype=int)
-        cnt = 0
-        up_bound = int(self.max_len * random.random())
-        if up_bound == 0:
-            up_bound = 1
-        while cnt < up_bound:
-            random_idx = np.random.randint(0, vec_len, dtype=int)
-            if mask[random_idx] == 0:
-                mask[random_idx] = 1
-                cnt = cnt + 1
-        return mask
+            self.mini_mine_sh(x, y, x_vars, y_var, max_x=max_x, verbose=verbose)
 
 
 def array2window(x, win_size=2):
@@ -132,3 +153,59 @@ def array2window(x, win_size=2):
     slices = np.array(slices)   # 将list转换为ndarray
     return slices
 
+
+def random_mask(vec_len, max_x):
+    """
+    随机生成长度为vec_len的01掩码串
+    :param max_x: 掩码中为1的最大位数，限制约束的复杂程度
+    :param vec_len: 生成的掩码长度
+    :return: 长度为vec_len的随机01掩码串
+    """
+    one_bits = random.randint(1, max_x)
+    mask_list = ['0' for i in range(vec_len)]
+    while mask_list.count('1') < one_bits:
+        random_bit = random.randint(0, vec_len-1)
+        mask_list[random_bit] = '1'
+    return ''.join(mask_list)
+
+
+def str2mask(mask_str):
+    try:
+        mask = [int(char) for char in mask_str]
+        return mask
+    except ValueError:
+        return None
+
+
+class Rule:
+    def __init__(self, x_names, y_name, func, lb, ub, model):
+        self.x_names = x_names
+        self.y_name = y_name
+        self.func = func
+        self.lb = lb
+        self.ub = ub
+        self.model = model
+
+    def violation_degree(self, x, y):
+        """
+        计算约束违反程度
+        :param x: 变量X集合的数据
+        :param y: 变量y的值
+        :return:
+        """
+        f = self.model.predict(x) - y   # 计算函数值f
+        if self.lb <= f <= self.ub:
+            return 0.
+        elif f < self.lb:
+            return self.lb - f
+        else:
+            return f - self.ub
+
+    def __str__(self):
+        f = '{} <= '.format(round(self.lb, 2))
+        for i in range(len(self.x_names)):
+            f = f + '{}*t{}[{}]'.format(round(self.func['coef'][i], 2), self.x_names[i][0], self.x_names[i][1])
+            f = f + ' + '
+        f = f + ' ({}) '.format(round(self.func['intercept'], 2))
+        f = f + ' - (t{}[{}]) <= {}'.format(self.y_name[0], self.y_name[1], round(self.ub, 2))
+        return f
--- constraints/stcd/mining.py
+++ constraints/stcd/mining.py
@@ -3,8 +3,8 @@ from constraints.stcd.linear import Linear
 
 def mining_stcd(mts, win_size=2, confidence=0.95, n_components=1, max_len=3,
                 sample_train=1.0, pc=1.0, ridge_all=False, implication_check=False,
-                verbose=0):
+                max_x=5, verbose=0):
     stcd = Linear(mts, win_size, confidence, n_components, max_len,
                   sample_train, pc, ridge_all, implication_check)
-    stcd.mine(verbose=verbose)
-    return None
+    stcd.mine(max_x=max_x, verbose=verbose)
+    return stcd.rules
